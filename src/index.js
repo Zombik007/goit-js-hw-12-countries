1597aaf03be7d2907edd7cc2baac143f5c2ffde3
Vitaliy Myts <74954880+Zombik007@users.noreply.github.com>
2021-06-09 21:23:29 +0300

    Добавляет файлы исходного кода

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
 import './sass/main.scss';
 import './js/fetchCountries';
-
-// import countryCardTpl from './templates/country-card.hbs';
-
-// fetch('https://restcountries.eu/rest/v2/name/mexico')
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(countries => {
-//     console.log(countries);
-//     const markup = countryCardTpl(countries);
-//     console.log(markup);
-//   })
-//   .catch(error => console.log(error));
